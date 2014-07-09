e837cc79ec13a34a44a93d4acab26062534a9519
Keegan Lambert <kqlambert@gmail.com>
2014-07-09 09:23:42 -0700

    ensure no horizontal scroll bar

--- js/build/scripts.js
+++ js/build/scripts.js
@@ -2,6 +2,17 @@
 // GENERAL
 ------------------------------------------------ */
 /*global $*/
+/*global console*/
 /*node browser: true*/
 'use strict';
 var b = $('body');
+// ensure no horizontal scrolling
+var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(
+	document.querySelectorAll('*'),
+	function(el) {
+		if (el.offsetWidth > docWidth) {
+			console.log(el);
+		}
+	}
+);
