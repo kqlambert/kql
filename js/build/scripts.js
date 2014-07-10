f225f469b207564a3b484e474da06285c6394de1
Keegan Lambert <kqlambert@gmail.com>
2014-07-10 09:15:44 -0700

    removed a hardly used function

--- js/build/scripts.js
+++ js/build/scripts.js
@@ -6,13 +6,3 @@
 /*node browser: true*/
 'use strict';
 var b = $('body');
-// ensure no horizontal scrolling
-/*var docWidth = document.documentElement.offsetWidth;
-[].forEach.call(
-	document.querySelectorAll('*'),
-	function(el) {
-		if (el.offsetWidth > docWidth) {
-			console.log(el);
-		}
-	}
-);*/
