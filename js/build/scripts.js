0d005952958a73cde0f7e3f54fb2d8e5d3567a54
kqlambert <kqlambert@gmail.com>
2015-06-15 12:20:19 -0700

    some changes

--- js/build/scripts.js
+++ js/build/scripts.js
@@ -12,21 +12,3 @@ $(window).load(function () {
 	$('.wrap').removeClass('preload');
 	$('.hero .wrapper').height(winH);
 });
-
-// Ripple (element must have the classes .js-ripple and .c-ripple to work)
-var $ripple = $('.js-ripple');
-$ripple.on('click.ui.ripple', function (e) {
-	var $this = $(this),
-		$offset = $this.parent().offset(),
-		$circle = $this.find('.c-ripple__circle'),
-		x = e.pageX - $offset.left,
-		y = e.pageY - $offset.top;
-	$circle.css({
-		top: y + 'px',
-		left: x + 'px'
-	});
-	$this.addClass('is-active');
-});
-$ripple.on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', function(e) {
-	$(this).removeClass('is-active');
-});
