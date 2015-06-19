9cd6559562c2e8e549c2f5dbeabf57e40580d671
kqlambert <kqlambert@gmail.com>
2015-06-19 14:42:30 -0700

    added google material ripple, also few small changes

--- js/build/scripts.js
+++ js/build/scripts.js
@@ -5,10 +5,27 @@
 /*global console*/
 /*node browser: true*/
 'use strict';
-var b = $('body'),
-	winH = $(window).height() - 1;
 $(window).load(function () {
 	$('.load').fadeOut();
 	$('.wrap').removeClass('preload');
-	$('.hero .wrapper').height(winH);
+});
+
+// ripple
+$('.ripple').on('click', function (e) {
+	e.preventDefault();
+	var x = e.pageX;
+		y = e.pageY,
+		el = $(this),
+		clickY = y - el.offset().top,
+		clickX = x - el.offset().left;
+	el.find('svg').remove();
+	el.append('<svg><circle cx="' + parseInt(clickX) + '" cy="' + parseInt(clickY) + '" r="0"></circle></svg>');
+	setTimeout(function () {
+		var c = el.find("circle");
+		c.animate({
+			'r' : el.outerWidth()
+		}, 400, function () {
+			el.find('svg').fadeOut('fast');
+		});
+	});
 });
