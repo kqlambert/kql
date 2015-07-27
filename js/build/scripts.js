e883ee792dd70923c8f7e02185e7f1908444687a
kqlambert <kqlambert@gmail.com>
2015-07-27 15:01:38 -0700

    rid of ripple

--- js/build/scripts.js
+++ js/build/scripts.js
@@ -10,22 +10,6 @@ $(window).load(function () {
 	$('.wrap').removeClass('preload');
 });
 
-// ripple
-$('.ripple').on('click', function (e) {
-	e.preventDefault();
-	var x = e.pageX;
-		y = e.pageY,
-		el = $(this),
-		clickY = y - el.offset().top,
-		clickX = x - el.offset().left;
-	el.find('svg').remove();
-	el.append('<svg><circle cx="' + parseInt(clickX) + '" cy="' + parseInt(clickY) + '" r="0"></circle></svg>');
-	setTimeout(function () {
-		var c = el.find("circle");
-		c.animate({
-			'r' : el.outerWidth()
-		}, 400, function () {
-			el.find('svg').fadeOut('fast');
-		});
-	});
-});
+/* ------------------------------- *\
+	# ???
+\* ------------------------------- */
